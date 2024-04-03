9a748f719d4746f58bd24989b16421db818a5073
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-04-03 23:46:18 +0530

    .

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -7,32 +7,32 @@ export default function Document() {
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/layout/favicon/apple-touch-icon.png'
+          href='/favicon/apple-touch-icon.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='32x32'
-          href='/layout/favicon/favicon-32x32.png'
+          href='/favicon/favicon-32x32.png'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='16x16'
-          href='/layout/favicon/favicon-16x16.png'
+          href='/favicon/favicon-16x16.png'
         />
-        <link rel='manifest' href='/layout/favicon/site.webmanifest' />
+        <link rel='manifest' href='/favicon/site.webmanifest' />
         <link
           rel='mask-icon'
-          href='/layout/favicon/safari-pinned-tab.svg'
+          href='/favicon/safari-pinned-tab.svg'
           color='#5bbad5'
         />
-        <link rel='shortcut icon' href='/layout/favicon/favicon.ico' />
+        <link rel='shortcut icon' href='/favicon/favicon.ico' />
         <meta name='msapplication-TileColor' content='#da532c' />
         <meta name='theme-color' content='#000000' />
         <meta
           name='msapplication-config'
-          content='/layout/favicon/browserconfig.xml'
+          content='/favicon/browserconfig.xml'
         />
       </Head>
 
