3a00ff0bed0cb4c5365fa20cd795d6eb2972de8e
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-04-09 00:08:47 +0530

    .

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
@@ -13,10 +12,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Unbounded:wght@200..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>
-        <Navbar/>
+        <Navbar />
         {children}
-        </body>
+      </body>
     </html>
   );
 }
