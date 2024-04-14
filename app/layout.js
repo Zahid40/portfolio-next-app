9f324945d9a83bc0301d44b11ea24f49a2b7026f
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-04-14 21:25:57 +0530

    new logo

--- app/layout.js
+++ app/layout.js
@@ -13,36 +13,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
-
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/android-chrome-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="512x512"
-          href="/android-chrome-512x512.png"
-        />
-
         <link
           rel="apple-touch-icon"
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
-
-        <link rel="shortcut icon" href="/favicon.ico" />
-
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/mstile-144x144.png" />
-        <meta name="msapplication-config" content="/browserconfig.xml" />
-
-        <meta name="theme-color" content="#ffffff" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#121212" />
+        <meta name="msapplication-TileColor" content="#121212" />
+        <meta name="theme-color" content="#121212" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
