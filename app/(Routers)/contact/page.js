20ec1a73bf35868dd2afccf68d4a772589ee9827
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-05-25 17:04:20 +0530

    Update page.js

--- app/(Routers)/contact/page.js
+++ app/(Routers)/contact/page.js
@@ -52,7 +52,7 @@ function Contact() {
   return (
     <div className="rounded-xl overflow-hidden w-full h-full flex flex-col items-center justify-center">
       <h1 className="text-6xl md:text-8xl pt-10 md:pt-20 pb-8 text-center">Contact Me<span className="text-zprimary-600 text-9xl leading-0">.</span></h1>
-      <p className="font-light text-base md:text-xl text-center w-5/6 md:w-2/3 pb-8 md:pb-12">I'd love to hear from you! Whether you have a question, a project idea, or just want to say hello, feel free to get in touch using the form below.</p>
+      <p className="font-light text-base md:text-xl text-center w-5/6 md:w-2/3 pb-8 md:pb-12">I&apos;d love to hear from you! Whether you have a question, a project idea, or just want to say hello, feel free to get in touch using the form below.</p>
       <div className="w-full h-full  flex md:flex-row justify-center md:p-8 p-4 flex-col">
         <Image
           src="/contactformimage.jpg"
