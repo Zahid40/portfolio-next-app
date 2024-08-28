e40f3e22ee9f5289db5d206b93a614f8e1ecd1d7
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-08-28 22:10:54 +0530

    Seo Updates

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { MyEmail, MyPhone, appname, description, keywords, url } from "@/const";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -12,73 +13,105 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  metadataBase: new URL('https://zahid.vercel.app'),
-  title: "Zahid 's Portfolio",
-  description: "Zahid's portfolio showcasing design skills and projects.",
-  authors: [{ name: 'Zahid' }],
-  creator: 'Zahid',
-  publisher: 'Zahid',
-  keywords: ['Next.js', 'React', 'JavaScript'],
+  metadataBase: new URL(url),
+  title: appname,
+  description: description,
+  authors: [{ name: "Zahid" }],
+  creator: "Zahid",
+  publisher: "Zahid",
+  keywords: keywords,
 
   icons: {
     icon: [
-      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/icons/icon.png' },
-      new URL('/icons/icon.png', 'https://zahid.vercel.app'),
-      { url: '/icons/icon-dark.png', media: '(prefers-color-scheme: dark)' },
+      { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/icon.png" },
+      new URL("/icons/icon.png", url),
+      { url: "/icons/icon.png", media: "(prefers-color-scheme: dark)" },
     ],
-    shortcut: ['/icons/shortcut-icon.png'],
+    shortcut: ["/icons/shortcut-icon.png"],
     apple: [
-      { url: '/icons/apple-icon.png', sizes: '180x180', type: 'image/png' },
-      { url: '/icons/apple-icon-x3.png', sizes: '180x180', type: 'image/png' },
+      {
+        url: "/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+      {
+        url: "/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
     ],
     other: [
       {
-        rel: 'apple-touch-icon-precomposed',
-        url: '/icons/apple-touch-icon-precomposed.png',
+        rel: "apple-touch-icon-precomposed",
+        url: "/icons/apple-touch-icon.png",
       },
-      { 
-        rel: 'mask-icon', 
-        url: '/icons/safari-pinned-tab.svg', 
-        color: '#5bbad5' 
+      {
+        rel: "mask-icon",
+        url: "/icons/safari-pinned-tab.svg",
+        color: "#5bbad5",
       },
     ],
   },
   msapplication: {
-    TileColor: '#da532c',
+    TileColor: "#da532c",
   },
-  manifest: '/site.webmanifest',
-
-
+  manifest: "/site.webmanifest",
 
   openGraph: {
-    title: "Zahid 's Portfolio",
-    description: "Zahid's portfolio showcasing design skills and projects.",
-    url: 'https://zahid.vercel.app',
-    siteName: "Zahid 's Portfolio",
-    images: '/images/ogimage.png',
-    type: 'website',
+    title: appname,
+    description: description,
+    url: url,
+    siteName: appname,
+    images: "/images/ogimage.png",
+    type: "website",
   },
   twitter: {
-    card: 'summary_large_image',
-    title: "Zahid 's Portfolio",
-    description: "Zahid's portfolio showcasing design skills and projects.",
-    siteId: '1467726470533754880',
-    creator: '@zahid',
-    creatorId: '1467726470533754880',
-    images: ['https://zahid.vercel.app/images/ogimage.png'], // Must be an absolute URL
+    card: "summary_large_image",
+    title: appname,
+    description: description,
+    creator: "@its_zahid83",
+    images: [`${url}/images/ogimage.png`], // Must be an absolute URL
   },
-  
-
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning >
+    <html lang="en" suppressHydrationWarning>
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Zahid",
+              "jobTitle": "Web Developer & Designer",
+              "description":  ${description},
+              "url":  ${url},
+              "sameAs": [
+                "https://www.linkedin.com/in/yourprofile",
+                "https://twitter.com/yourprofile",
+                "https://github.com/yourprofile"
+              ],
+              "worksFor": {
+                "@type": "Organization",
+                "name": "Zahid Web Solutions",
+                "url": ${url}
+              },
+              "knowsAbout": ${keywords},
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "contactType": "Customer Service",
+                "email": ${MyEmail},
+                "telephone": ${MyPhone},
+                "url": {${url}/contact}
+              }
+            }
+          `}
+        </script>
       </head>
       <body className={poppins.className}>
         <div className="flex items-center justify-center">
---
+++ const.js
@@ -0,0 +1,41 @@
+//App 
+export const appname = "Zahid 's Portfolio";
+export const url = "https://zahid.vercel.app"
+export const description = "At Zahid Web Solutions, I offer top-tier web development services, specializing in cutting-edge technologies like Next.js, React.js, and JavaScript. Whether you're looking to build a responsive website, a dynamic web application, or need a web designer skilled in Figma, I have the expertise to deliver. My services include JavaScript development, Next.js web applications, React.js front-end development, and custom web design. Let’s create a modern website that’s fast, scalable, and visually appealing, tailored to your business needs."
+export const keywords = [
+    'Next.js',
+    'React',
+    'JavaScript',
+    'Web Development',
+    'Frontend Development',
+    'Web Design',
+    'Figma',
+    'Responsive Web Design',
+    'UI/UX Design',
+    'HTML5',
+    'CSS3',
+    'Node.js',
+    'Full-Stack Development',
+    'Progressive Web Apps',
+    'Single Page Applications',
+    'API Integration',
+    'Web Animation',
+    'Interactive Web Design',
+    'SEO-Friendly Websites',
+    'Website Optimization',
+    'Cross-Browser Compatibility',
+    'Web Accessibility',
+    'Web Application Development',
+    'JavaScript Frameworks',
+    'Component-Based Architecture',
+    'Modern Web Technologies',
+    'Mobile-First Design',
+    'User-Centric Design',
+    'Digital Product Design'
+  ]
+
+//Social Information
+export const MyEmail  = "zahidanasari17@gmail.com"
+export const MyPhone  = "+91 9625162488"
+export const Instagram  = "+91 9625162488"
+  
