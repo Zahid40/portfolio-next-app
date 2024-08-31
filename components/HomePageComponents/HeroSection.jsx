0426c7eb9d57a588da38c71fa1495dedd517bf66
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-08-31 21:26:28 +0530

    Update HeroSection.jsx

--- components/HomePageComponents/HeroSection.jsx
+++ components/HomePageComponents/HeroSection.jsx
@@ -6,24 +6,6 @@ import Typed from "typed.js";
 import { FiArrowUpRight, FiDownload } from "react-icons/fi";
 
 function HeroSection() {
-  const el = useRef(null);
-
-  useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: ["Developer", "Designer"],
-      typeSpeed: 0,
-      backSpeed: 0,
-      fadeOut: true,
-      cursorChar: "",
-      backDelay: 5000,
-      loop: true,
-    });
-
-    return () => {
-      typed.destroy();
-    };
-  }, []);
-
   return (
     <>
       <div className="py-12 md:py-24  flex flex-col justify-between relative  z-20 backdrop-blur-[140px]  bg-contain bg-no-repeat ">
@@ -31,8 +13,8 @@ function HeroSection() {
           <div className=" flex flex-col md:flex-row  justify-center items-center gap-6 ">
             <div className=" flex flex-col justify-center items-center gap-6">
               <h2 className=" md:text-8xl text-6xl text-center">
-                {"{ "}Creative & Experienced {" "}
-                 <br/>Web Developer {" }"}
+                {"{ "}Creative & Experienced <br />
+                Web Developer {" }"}
               </h2>
               <p className="text-xs md:text-base font-normal text-center w-2/3 text-neutral-300 tracking-wide">
                 In addition to being a web developer, I am also a skilled web
