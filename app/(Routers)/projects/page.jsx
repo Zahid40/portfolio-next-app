36e582977eb76e8b5bbe873e8c7308ec19a5886e
Techmier <72434947+Zahid40@users.noreply.github.com>
2024-09-25 12:06:05 +0530

    Update page.jsx

--- app/(Routers)/projects/page.jsx
+++ app/(Routers)/projects/page.jsx
@@ -20,7 +20,6 @@ export const metadata = {
       }
     ],
     type: "website",
-    
   },
   twitter: {
     card: "summary_large_image",
@@ -31,20 +30,17 @@ export const metadata = {
   },
 };
 
-
 async function Projects() {
   const data = c_data.projects;
-
   return (
     <div className="rounded-xl overflow-hidden h-full w-full  pb-12 pt-[10vh] px-main max-w-6xl m-auto min-h-svh  relative ">
       <div className="border-2 p-8 rounded-sm">
-      <h1 className="text-clamp-6xl  ">Projects</h1>
+      <h1 className="text-clamp-6xl">Projects</h1>
       <p className="font-light text-sm w-full md:w-2/3">DotoZ is a powerful todo app designed to help you organize tasks, set priorities, and boost productivity. It&apos;s available as a downloadable web app, created with React + Vite.</p>
-
       </div>
       <div className="w-full h-full flex justify-center md:p-8 p-4 flex-col gap-12 mx-auto ">
         {Object.keys(data).map((projectName) => (
-          <Card key={projectName} data={data[projectName]} /> 
+          <Card key={projectName} data={data[projectName]} />
         ))}
       </div>
     </div>
